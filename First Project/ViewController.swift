9773e69099560e2f9187c8e42edd20a8ce39f5ab
scott <zaxaz@mac.com>
2020-01-15 20:56:00 -0700

    Initial Commit

---
+++ First Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Todd Perkins on 11/11/19.
+//  Copyright © 2019 Todd Perkins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
